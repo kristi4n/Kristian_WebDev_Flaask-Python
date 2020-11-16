1f106c24066d45a699dceb405d53600cbcb725ef
kristi4n <57930350+kristi4n@users.noreply.github.com>
2020-11-16 19:07:41 +0800

    Semi Complete
    Masih kurang di sistem login

--- app/controllers/indexcontroller.py
+++ app/controllers/indexcontroller.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, session
 from app import app
 from app.models.user import User ## import kelas User dari model
 
@@ -8,17 +8,72 @@ from app.models.user import User ## import kelas User dari model
 # 	nama = user.getName() ## memanggil method untuk mengambil nama
 # 	return render_template('index.html', nama=nama)
 
+app.secret_key = "Latihan"
+authorize = False
 @app.route('/')
 def index():
 	return render_template('index.html')
 
 @app.route('/login2', methods = ["GET", "POST"])
-def login():
-	if request.method == "POST":		
+def login():	
+	error = None
+	Data = {
+		"username" : "admin",
+		"name" : "Administrator",
+		"password" : "123" 
+	}
+	if session:
 		return redirect(url_for("index"))
 	else:
-		return render_template('login.html')
+		if request.method == "POST":
+			if Data["username"] == request.form["nama_user"]:
+				if Data["password"] == request.form["kata_sandi"]:
+					session["username"] = request.form["nama_user"]
+					session["name"] = "administrator"
+					return render_template('index.html', session = session)
+				else:
+					error = "Password Salah"
+					nama = request.form["nama_user"]
+					return render_template('login.html', error = error, nama = nama)
+			else:
+				error = "Username tidak terdaftar"
+				return render_template('login.html', error = error)
+		else:
+			return render_template('login.html')
+			
+@app.route('/authenticate')
+def authenticate():
+	error = None
+	Data = {
+		"username" : "admin",
+		"name" : "Administrator",
+		"password" : "123" 
+	}
+	
+	if Data["username"] == request.form["nama_user"]:
+		if Data["password"] == request.form["kata_sandi"]:
+			return render_template('index.html', session = session)
+		else:
+			error = "Password Salah"
+			return render_template('login.html', error)
+	else:
+		error = "Username tidak terdaftar"
+		return render_template('login.html', error)
+
 
 @app.route('/register2')
 def register():
 	return render_template('register.html')
+
+@app.route('/logout')
+def logout():
+	session.pop("username")
+	session.pop("name")	
+	return redirect(url_for("index"))
+
+@app.route('/useronly')
+def useronly():
+	if session:
+		return render_template('user_only.html')
+	else:
+		
